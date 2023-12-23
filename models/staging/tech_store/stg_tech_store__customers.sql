8e8f0097df0ac7b34ffe5cc484ddac4a8c2d02ab
hoanghce <nhhoang.hce@gmail.com>
2023-12-23 16:39:57 +0700

    hoang update

--- models/staging/tech_store/stg_tech_store__customers.sql
+++ models/staging/tech_store/stg_tech_store__customers.sql
@@ -15,8 +15,10 @@ final as (
         mainsalesrepid as main_employee_id,
         createdatetime as created_at,
         convert_timezone('UTC','America/New_York',createdatetime) as created_at_est,
+        {{ utc_to_est('createdatetime') }} as created_at_est_h,
         updatedatetime as updated_at,
-        convert_timezone('UTC','America/New_York',updatedatetime) as update_at_est,   
+        convert_timezone('UTC','America/New_York',updatedatetime) as update_at_est,
+        {{ utc_to_est('update_at_est') }} as update_at_est_h,
         active as is_active
     
     from customers
